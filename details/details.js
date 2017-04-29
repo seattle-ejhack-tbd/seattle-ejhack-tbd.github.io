eccc34be88503d7da5c713e18197dc1eafba4d8f
Brian Lee <brianjleeofcl@gmail.com>
2017-04-29 12:24:01 -0700

    fix broken json

--- details/details.js
+++ details/details.js
@@ -1,9 +1,14 @@
 (function() {
   const id = new URL(window.location).searchParams.get('id');
-  console.log(id);
 
-  $.getJSON(`https://raw.githubusercontent.com/seattle-ejhack-tbd/seattle-ejhack-tbd.github.io/master/bill/${id}`).done(data => {
-    console.log(data)
+  $.getJSON(`https://raw.githubusercontent.com/seattle-ejhack-tbd/seattle-ejhack-tbd.github.io/master/bill/${id}`).done(({title, introduced_at, sponsor, summary, text_link }) => {
+    const paragraphs = summary.split('\n');
+    $('#loading').remove()
+    const $details = $('#details');
+    const $title = $('<h4>').text(title)
+    const $summary = paragraphs.map(paragraph => $('<p>').text(paragraph));
+    $details.append($title, $summary);
+
   }).fail(console.error)
 
 
