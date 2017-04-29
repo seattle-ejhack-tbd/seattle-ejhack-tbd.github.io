f04436aa0fdec6812a31dc4092d0f8dbefb0075e
Brian Lee <brianjleeofcl@gmail.com>
2017-04-29 14:14:34 -0700

    CSS

--- details/details.js
+++ details/details.js
@@ -2,12 +2,15 @@
   const id = new URL(window.location).searchParams.get('id');
 
   $.getJSON(`https://raw.githubusercontent.com/seattle-ejhack-tbd/seattle-ejhack-tbd.github.io/master/bill/${id}`).done(({title, introduced_at, sponsor, summary, text_link }) => {
-    const paragraphs = summary.split('\n');
     $('#loading').remove()
     const $details = $('#details');
     const $title = $('<h4>').text(title)
-    const $summary = paragraphs.map(paragraph => $('<p>').text(paragraph));
-    $details.append($title, $summary);
+    const $summary = $('<p>').text(summary);
+    const $sponsor = $('<p>').text(`Sponsored by ${sponsor}`);
+    const $time = $('<p>').text(`Introduced ${introduced_at}`);
+    const $link = $('<a>').attr('href', text_link).text('Full text');
+    $details.append($title, $summary, $sponsor, $time, $link);
+
 
   }).fail(console.error)
 
